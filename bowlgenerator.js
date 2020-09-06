f38668e4ca8bfbd6b29737da8dcd167e4d62b63e
lucymk <lucy.m.king92@gmail.com>
2020-09-06 17:19:35 +0100

    add files

---
+++ bowlgenerator.js
@@ -0,0 +1,216 @@
+const grains = {
+  Rice: "https://thewanderlustkitchen.com/how-to-cook-perfect-rice/",
+  Couscous:
+    "https://www.delish.com/uk/food-news/a29471283/how-to-cook-couscous/",
+  Quinoa: "https://cookieandkate.com/perfect-quinoa/",
+  "Bulgar wheat":
+    "https://www.bbcgoodfood.com/howto/guide/how-cook-bulgur-wheat",
+  Barley:
+    "https://www.thekitchn.com/how-to-cook-barley-cooking-lessons-from-the-kitchn-198693",
+};
+
+const leafyGreens = {
+  Spinach: "/spinach",
+  Rocket: "/rocket",
+  "Mixed leaves": "/mixed-leaves",
+  Lettuce: "/lettuce",
+  Kale: "https://www.thekitchn.com/how-to-cook-kale-261979",
+};
+
+const nutsAndSeeds = {
+  "Pumpkin seeds": "https://www.youtube.com/watch?v=QbxqCYW2_eg",
+  "Sesame seeds": "https://www.youtube.com/watch?v=tkr9gv7gFcc",
+  "Sunflower seeds": "https://www.youtube.com/watch?v=QbxqCYW2_eg",
+  Almonds: "https://www.youtube.com/watch?v=TWrtMdz_bgI",
+  Hazelnuts: "https://www.youtube.com/watch?v=Yo0IbuEaWU4",
+  Walnuts: "https://www.youtube.com/watch?v=bMEQkKzhOxM",
+  Pecans: "https://www.youtube.com/watch?v=bMEQkKzhOxM",
+  Cashews: "https://www.youtube.com/watch?v=bMEQkKzhOxM",
+  "Pine nuts": "https://www.youtube.com/watch?v=NKD0KMjNbrw",
+};
+
+const sweetThings = {
+  Dates: "https://www.youtube.com/watch?v=fMoCmU315bc",
+  Raisins: "https://www.youtube.com/watch?v=fMoCmU315bc",
+  Cranberries: "https://www.youtube.com/watch?v=fMoCmU315bc",
+  "Dried apricots": "https://www.youtube.com/watch?v=fMoCmU315bc",
+  Apple: "https://www.youtube.com/watch?v=8AyPfkbgz8s",
+};
+
+const sauces = {
+  Tahini: "https://cookieandkate.com/best-tahini-sauce-recipe/",
+  "Green goddess":
+    "https://cookieandkate.com/easy-green-goddess-dressing-recipe/",
+  "Ginger & turmeric tahini":
+    "https://starseedkitchen.com/turmeric-ginger-tahini-dressing/",
+  Satay: "https://minimalistbaker.com/5-ingredient-peanut-sauce/",
+  "Lemon, honey & mustard":
+    "https://cookieandkate.com/healthy-honey-mustard-dressing-recipe/",
+  "Curry tahini":
+    "https://quitegoodfood.co.nz/potato-cauliflower-lentil-salad-curry-tahini-dressing/",
+  "Thai red curry":
+    "https://www.thefullhelping.com/five-ingredient-red-curry-coconut-sauce-or-dressing/",
+  "Soy, ginger & honey":
+    "https://www.allrecipes.com/recipe/49923/asian-ginger-dressing/",
+  Pesto:
+    "https://www.bonappetit.com/test-kitchen/how-to/article/pesto-out-of-anything",
+};
+
+const rootVeg = {
+  "Sweet potato":
+    "https://www.delish.com/uk/cooking/recipes/a30311290/how-to-roast-sweet-potatoes/",
+  Potatoes:
+    "https://www.spendwithpennies.com/simple-herb-oven-roasted-potatoes/",
+  Plantain: "https://healthiersteps.com/recipe/baked-plantains/",
+  Carrots: "https://cookieandkate.com/perfect-roasted-carrots-recipe/",
+  Parsnip:
+    "https://www.delish.com/uk/cooking/recipes/a29871209/roast-parsnips-recipe/",
+  Beetroot:
+    "https://morethanjustcarrots.com/roasted-beetroot-with-honey-and-balsamic/",
+  "Butternut squash": "https://www.youtube.com/watch?v=5f3MdyAg2aM",
+};
+
+const otherVeg = {
+  Peppers: "",
+  Brocolli: "https://cookieandkate.com/perfect-roasted-broccoli-recipe/",
+  Cauliflower: "https://cookieandkate.com/roasted-cauliflower-recipe/",
+  Cabbage: "https://www.youtube.com/watch?v=p5ULTwk9H08",
+  Sweetcorn: "/sweetcorn",
+  Onions: "https://healthyrecipesblogs.com/roasted-red-onions/",
+  "Spring greens": "https://www.youtube.com/watch?v=xKFTHzjYNM4",
+  Aubergine: "https://www.melaniecooks.com/roasted-eggplant-cubes/12587/",
+  Sugarsnaps: "https://www.youtube.com/watch?v=QoQeWREUpVE",
+  "Green beans": "https://www.spendwithpennies.com/roasted-green-beans/",
+  Courgette: "https://www.peelwithzeal.com/roasted-zucchini/",
+  Tomatoes: "https://www.cookinglight.com/recipes/roasted-cherry-tomatoes",
+};
+
+const protein = {
+  Tofu: "https://cookieandkate.com/how-to-make-crispy-baked-tofu/",
+  TVP:
+    "https://theveganhoneypot.ie/2019/09/30/how-to-prep-soya-chunks-for-plantmeaty-recipes/",
+  "Veggie sausage": "",
+  "Kidney beans": "",
+  Lentils: "https://www.acouplecooks.com/how-to-cook-lentils/",
+  Chickpeas: "",
+  "Black beans": "",
+  "Butter beans": "",
+};
+
+const herbs = {
+  Parsley: "",
+  Mint: "",
+  Dill: "",
+  Coriander: "",
+  Basil: "",
+};
+
+const freshThings = {
+  Avocado: "",
+  "Spring onion": "",
+  Cucumber: "",
+  Radish: "",
+  Tomato: "",
+};
+
+const extras = {
+  Pickles: "",
+  Hummus: "",
+  Capers: "",
+  Olives: "",
+  "Sundried tomatoes": "",
+};
+
+const always = {
+  ingredient:
+    "Squeeze of lemon or lime, salt and pepper and el ingrediente secreto ❤️",
+};
+
+const random = (obj) => {
+  var keys = Object.keys(obj);
+  const ingredient = keys[(keys.length * Math.random()) << 0];
+  const info = obj[ingredient];
+
+  return { ingredient, info };
+};
+
+const random2 = (obj) => {
+  var keys = Object.keys(obj);
+  const ingredient = keys[(keys.length * Math.random()) << 0];
+  let ingredient2 = keys[(keys.length * Math.random()) << 0];
+
+  while (ingredient == ingredient2) {
+    ingredient2 = keys[(keys.length * Math.random()) << 0];
+  }
+
+  const info = obj[ingredient];
+  const info2 = obj[ingredient2];
+
+  const both = [
+    { ingredient, info },
+    { ingredient: ingredient2, info: info2 },
+  ];
+
+  const both2 = {
+    ingredient: `${ingredient} and ${ingredient2}`,
+    info: info,
+    info2: info2,
+  };
+
+  return both2;
+};
+
+const bowl = () => {
+  let myBowl = {
+    "Your base grain": random(grains),
+    "Greens are important": random(leafyGreens),
+    "Root veggie": random(rootVeg),
+    "Other veggies": random2(otherVeg),
+    "Something sweet": random(sweetThings),
+    "Nut or seed": random(nutsAndSeeds),
+    Protein: random(protein),
+    "A bit of...": random(herbs),
+    "Something fresh": random(freshThings),
+    "Plus..": random(extras),
+    "Make it yummy with a sauce": random(sauces),
+    "Don't forget": always,
+  };
+
+  Object.keys(myBowl).map((item, index) => {
+    const prevItem = document.getElementById("list");
+
+    const el = document.createElement("div");
+    el.innerHTML = `${item}: `;
+    el.className = "item";
+
+    document.getElementById(`${index + 1}`)
+      ? prevItem.replaceChild(el, document.getElementById(`${index + 1}`))
+      : prevItem.appendChild(el);
+
+    el.id = `${index + 1}`;
+
+    const p = document.createElement("span");
+
+    p.innerHTML = `${myBowl[item].ingredient} `;
+    p.className = "ingredient";
+    el.appendChild(p);
+
+    const info = document.createElement("a");
+    info.innerHTML = "ℹ️";
+    info.className = "info";
+    info.href = myBowl[item].info;
+    info.target = "_blank";
+    el.appendChild(info);
+
+    console.log("bowl", myBowl[item]);
+
+    if ("info2" in myBowl[item]) {
+      const info2 = document.createElement("a");
+      info2.innerHTML = " ℹ️";
+      info2.target = "_blank";
+      info2.className = "info";
+      info2.href = myBowl[item].info2;
+      el.appendChild(info2);
+    }
+  });
+};
